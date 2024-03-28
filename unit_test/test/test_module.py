b428528ca7d7def53749a3f122873753b65f8107
topcan5 <gesong5@gmail.com>
2024-03-28 00:52:36 -0700

    run works, but debug doesn't.

--- unit_test/test/test_module.py
+++ unit_test/test/test_module.py
@@ -1,7 +1,7 @@
   # The code to test
 import unittest   # The test framework
-import module
-# import sys, os
+from unit_test import g_module
+import sys, os
 
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
 # sys.path.append('app/')
@@ -13,7 +13,7 @@ import module
 class TestMyModule(unittest.TestCase):
     def test_add(self):
         # print("Current Working Directory:", os.getcwd())
-        self.assertEqual(module.add(1,3), 4)
+        self.assertEqual(g_module.add(1,3), 4)
 
 if __name__ == '__main__':
     unittest.main()
