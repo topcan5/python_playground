1f453300e0210aeaea0df5185741c0ccffe27f89
topcan5 <gesong5@gmail.com>
2024-03-27 23:14:49 -0700

    works in module

---
+++ unittest/module.py
@@ -0,0 +1,2 @@
+def add(a, b) -> int:
+    return a + b
--- unittest/test_module.py
+++ unittest/test_module.py
@@ -1,5 +1,6 @@
   # The code to test
 import unittest   # The test framework
+import module
 # import sys, os
 
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
@@ -12,7 +13,7 @@ import unittest   # The test framework
 class TestMyModule(unittest.TestCase):
     def test_add(self):
         # print("Current Working Directory:", os.getcwd())
-        self.assertEqual(4, 4)
+        self.assertEqual(module.add(1,3), 4)
 
 if __name__ == '__main__':
     unittest.main()
